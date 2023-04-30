168d09e345acdd94ba17feabe9475b0138701138
JohnDRD <jrodriguez@cidenet.com.co>
2023-04-29 19:14:13 -0500

    Initial version of ReportesGradosBE generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/udea/gr/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.udea.gr.service.dto;
---
+++ src/test/java/com/udea/gr/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.udea.gr;
+
+import com.udea.gr.ReportesGradosBeApp;
+import com.udea.gr.config.AsyncSyncConfiguration;
+import com.udea.gr.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ReportesGradosBeApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
